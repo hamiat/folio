bf6410b9216efcbdf6da7e108ea466a3ea50776c
hamiat <hamiat.n@hotmail.com>
2022-01-19 15:35:20 +0100

    working point

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,80 +1,63 @@
-import style from "./about.module.scss"
-import nameTwo from '../../nameTwo.svg'
-import { motion } from "framer-motion"
+import style from "./about.module.scss";
+import nameTwo from "../../nameTwo.svg";
+import { motion, Variants } from "framer-motion";
 
-/* const containerVariants = {
-  initial: {
-   
-    opacity:0,
+const contentVariants: Variants = {
+  offscreen: {
+    opacity:0
   },
-  animate: {
-    y:0,
-    opacity:1,
-    transition: {
-      type: 'spring', 
-      duration: 1.5,
-      mass: 500,
-      damping: 15,
-      when: "beforeChildren",
-      staggerChildren: 0.6
-    }
-  },exit: {
-    x: "-100vw",
-    transition: {
-      ease: "easeInOut"
-    }
+  onscreen: {
+   opacity: 1,
+   transition: {
+    ease: "easeInOut",
+    delay: 5.8,
+    duration: 2,
   }
-}
- */
-
-
-
-const childVariants = {
-  initial: {
-    y: "100vh",
+    
   },
-  animate: {
-    y:0, 
-  }
-}
-const reveal = {
-  initial: {
+};
+const titleVariants: Variants = {
+  offscreen: {
+    y: 0,
     opacity: 0,
   },
-  animate: {
-    opacity:1, 
-  }
-}
+  onscreen: {
+    y: "-4vh",
+    opacity: 1,
+    transition: {
+      ease: "easeInOut",
+      delay: 3.8,
+      duration: 2,
+      
+    },
+  },
+};
 
 const About: React.FC = () => {
-    return(
-
-<div className={style.about}  >
-
-  <motion.h2
-  variants={childVariants}
-  initial="initial"
-  animate="animate"
-  transition= {{
-    type: 'tween', 
-    delay: 5,
-    duration: 1.5,
-  }}
-  >
-    Hello there!</motion.h2>
-    <motion.div   variants={reveal}
-  initial="initial"
-  animate="animate" transition= {{
-    type: 'tween', 
-    delay: 6.5,
-    duration: 1.5,
-  }}>
-    <motion.h3>welcome to my website...</motion.h3>
-  <motion.p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui </motion.p>
-  </motion.div>
+  return (
+    <motion.div
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: false, amount: 0.8 }}
+      className={style.content}
   
-</div>
-    )
-}
+    >
+      <motion.section className={style.about} >
+      <motion.h2 variants={titleVariants} >Hello there!</motion.h2>
+      <motion.div >
+        <motion.h3 variants={contentVariants} 
+     >
+          welcome to my website...
+        </motion.h3>
+        <motion.p variants={contentVariants} >
+          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+          fugit, sed quia consequuntur magni dolores eos qui
+        </motion.p>
+      </motion.div>
+      </motion.section>
+     
+    </motion.div>
+  );
+};
 
-export default About 
+export default About;
