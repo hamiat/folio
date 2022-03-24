62268e7001c5695ee2cb7cf756586466077c8d6a
hamiat <hamiat.n@hotmail.com>
2022-03-24 13:35:34 +0100

    [wip] added a project

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -1,44 +1,44 @@
 import style from "./projects.module.scss";
 import { motion, Variants } from "framer-motion";
 import Button from "../Button/Button";
-import { type } from "@testing-library/user-event/dist/type";
+import { useState } from "react";
 
-
-
-export type IProps =  {
-  hidden: boolean
-  showProject: any
-  id: number
-  title: string
-  image: string
-  alt: string
-  text: string
-
-}
-
-
-const Project: React.FC<IProps> = ({id, title, image, alt, text, showProject, hidden}) => {
-
-
-  
-  return (
-  <div className={style.projectsDiv}>
-    <h3 className={style.companyName}>{title}</h3>
-     <Button  onClick={showProject}>{hidden ? "Show less" : "Show more"}</Button>
-     <div className={`${hidden ? style.showMore : style.showLess }`}>
-   
-   <img className={style.judilicaTwo} src={image} alt={alt} />
-   <img className={style.judilicaOne} src={image} alt={alt} />
-  
-   
-  
-   </div>
-
-   <p className={`${hidden ? style.showMore : style.showLess } ${style.judilicaText}`}>{text}</p>
-  </div>)
+export type IProps = {
+ index: number,
+  id: number;
+  title: string;
+  image: string;
+  alt: string;
+  text: string;
+  url: string;
+  urlText: string;
  
 };
 
+const Project: React.FC<IProps> = ({ id, title, image, alt, text, url , urlText, index }) => {
+  const [hidden, setHidden] = useState(false);
+  const showProject = () => {
+    setHidden(!hidden);
+  };
 
-export default Project
+  return (
+    <div className={style.projectsDiv}>
+      <h3  className={style.projectsTitle}>{title}</h3>
+      <button onClick={showProject}>
+        {hidden ? "Show less" : "Show more"}
+      </button>
+     
+      <div className={`${hidden ? style.showMore : style.showLess}`}>
+      <p  className={style.projectWrapper}> <a className={style.projectUrl} href={url}> {urlText}</a></p>
+        <img className={style.projectImage} src={image} alt={alt} />
+      </div>
+      <p className={`${hidden ? style.showMore : style.showLess} ${
+          style.projectText
+        }`}>
+        {text}
+      </p>
+    </div>
+  );
+};
 
+export default Project;
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,47 +1,40 @@
 import style from "./projects.module.scss";
 import { motion, Variants } from "framer-motion";
 import { useState } from "react";
-import Project, { IProps,  } from "./Project";
-
-
+import Project, { IProps } from "./Project";
 
 export default function Projects(): JSX.Element {
-  const [hidden, setHidden] = useState(false)  
-
-  const [projects, setProjects ] = useState<Omit<IProps, "hidden" |"showProject">[]>([
+  const [projects, setProjects] = useState<Omit<IProps, "index">[]>([
     {
-      id: 1 ,
+      id: 1,
+      title: "8 Great Repositories",
+      image: "/assets/repos.png",
+      alt: "desktop and mobile example of an app",
+      text: "A counter that uses the GitHub REST API to retrieve some information about 8 great repositories, created with React & JSS.",
+      url: "https://codesandbox.io/s/github/hamiat/eight-great-repos?file=/src/App.js:1866-1895",
+      urlText: "Try this out @ codesandbox",
+      
+    },
+    {
+      id: 2,
       title: "Judilica",
       image: "/assets/judilica2.jpg",
       alt: "desktop and mobile example of a website",
-      text: "Eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet. Consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore"
-    },
-    {
-      id: 2 ,
-      title: "8 Great Repositories",
-      image: "/assets/repos.jpg",
-      alt: "desktop and mobile example of an app",
-      text: "Eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet. Consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore"
-    },
-  ])
-
-  const showProject = () => {
-    setHidden(!hidden)
-  }
-  
-  return <motion.section className={style.projects}>
-    <h2>Some of my projects</h2>
-    <div>
+      text: "Eiusmod tempor incididunt ut labore Lorem ipsum dolor sit amet. Consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore",
+      url: "",
+      urlText: "",
    
-  
- 
- {projects.map( project => (
-   <Project {...project} hidden={hidden} showProject={showProject} />
- ))}
-    
-    
-    </div>
-  </motion.section>;
-};
+    },
+  ]);
 
- 
+  return (
+    <motion.section className={style.projects}>
+      <h2>Some of my projects</h2>
+      <div>
+        {projects.map((project, index) => (
+          <Project {...project} index={index} />
+        ))}
+      </div>
+    </motion.section>
+  );
+}
