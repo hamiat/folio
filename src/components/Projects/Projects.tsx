fa617fa834997b357c37f6f80c8be2032f3e4e8d
hamiat <70755521+hamiat@users.noreply.github.com>
2024-02-14 12:11:32 +0100

    Update Projects.tsx - Change order of games

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -21,15 +21,6 @@ const Projects: React.FC = () => {
     },
     {
       id: 2,
-      title: "8 Great Repositories",
-      image: "/assets/repos.png",
-      alt: "desktop and mobile example of an app",
-      text: "A counter that uses the GitHub REST API to retrieve some information about 8 great repositories, created with React & JSS.",
-      url: "https://codesandbox.io/s/github/hamiat/eight-great-repos?file=/src/App.js:1866-1895",
-      urlText: "Try this out @ codesandbox",
-    },
-    {
-      id: 3,
       title: "Space game",
       image: "/assets/spaces.png",
       alt: "desktop and mobile example of a website",
@@ -38,13 +29,21 @@ const Projects: React.FC = () => {
       urlText: "Try this out (for desktop)",
     },
     {
-      id: 4,
+      id: 3,
       title: "Interactive children's game",
       image: "/assets/oldmcdonld.png",
       alt: "desktop and mobile example of a website",
       text: "An interactive kids game based on the song 'Old McDonald had a farm'.",
       url: "https://creative-starburst-5e276c.netlify.app/",
       urlText: "Try this out (for desktop)",
+    },    {
+      id: 4,
+      title: "8 Great Repositories",
+      image: "/assets/repos.png",
+      alt: "desktop and mobile example of an app",
+      text: "A counter that uses the GitHub REST API to retrieve some information about 8 great repositories, created with React & JSS.",
+      url: "https://codesandbox.io/s/github/hamiat/eight-great-repos?file=/src/App.js:1866-1895",
+      urlText: "Try this out @ codesandbox",
     },
   ]);
 
